561ec3a2da7893608e021a79436ac8bf18374139
kosch104 <konstantin-schwarze@outlook.de>
2024-11-27 01:30:00 +0100

    First WIP Version

--- Mod.cs
+++ Mod.cs
@@ -1,17 +1,21 @@
-﻿using Colossal.Logging;
+﻿using System.Collections.Generic;
+using System.IO;
+using Colossal.Logging;
 using Game;
 using Game.Modding;
 using Game.SceneFlow;
 using Colossal.IO.AssetDatabase;
+using Colossal.PSI.Environment;
 
-namespace Region_Flag_Icons
+namespace RegionFlagIcons
 {
     public class Mod : IMod
     {
-        public static ILog log = LogManager.GetLogger($"{nameof(Region_Flag_Icons)}.{nameof(Mod)}")
+        public static ILog log = LogManager.GetLogger($"{nameof(RegionFlagIcons)}.{nameof(Mod)}")
             .SetShowsErrorsInUI(false);
 
         private Setting m_Setting;
+        private static string targetDirectory = Path.Combine(EnvPath.kUserDataPath, "ModsData", "AssetIconLibrary", "CustomThumbnails");
 
         public void OnLoad(UpdateSystem updateSystem)
         {
@@ -24,8 +28,59 @@ namespace Region_Flag_Icons
             m_Setting.RegisterInOptionsUI();
             GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_Setting));
 
+            if (!Directory.Exists(targetDirectory))
+            {
+                log.Error($"Target directory {targetDirectory} does not exist. Files could not be copied.");
+            }
+            else
+            {
+                List<string> directories = ["de_thumbnails", "uk_thumbnails", "fr_thumbnails", "flags"];
+                log.Info("AssetPath: " + asset.path);
+                foreach (string directory in directories)
+                {
+                    FileInfo fi = new FileInfo(asset.path);
+                    var path = Path.Combine(fi.Directory!.FullName, directory);
+                    log.Info("FullPath: " + path);
+                    CheckAndCopyFiles(path);
+                }
+            }
 
-            AssetDatabase.global.LoadSettings(nameof(Region_Flag_Icons), m_Setting, new Setting(this));
+            AssetDatabase.global.LoadSettings(nameof(RegionFlagIcons), m_Setting, new Setting(this));
+        }
+
+        private static void CheckAndCopyFiles(string directory)
+        {
+            DirectoryInfo di = new DirectoryInfo(directory);
+            if (!di.Exists)
+            {
+                log.Error($"Directory {directory} does not exist. Files could not be copied. Please re-download this mod");
+                return;
+            }
+
+            foreach (FileInfo fi in di.GetFiles())
+            {
+                // Check if files are equal, if not, overwrite
+                bool overwrite = false;
+                if (File.Exists(Path.Combine(targetDirectory, fi.Name)))
+                {
+                    if (fi.Length != new FileInfo(Path.Combine(targetDirectory, fi.Name)).Length)
+                    {
+                        overwrite = true;
+                    }
+                }
+                File.Copy(fi.FullName, Path.Combine(targetDirectory, fi.Name), overwrite);
+            }
+        }
+
+        public static void DeleteChangedIcons()
+        {
+            if (Directory.Exists(targetDirectory))
+            {
+                foreach (string file in Directory.GetFiles(targetDirectory))
+                {
+                    File.Delete(file);
+                }
+            }
         }
 
         public void OnDispose()
--- Setting.cs
+++ Setting.cs
@@ -6,79 +6,35 @@ using Game.UI;
 using Game.UI.Widgets;
 using System.Collections.Generic;
 
-namespace Region_Flag_Icons
+namespace RegionFlagIcons
 {
-    [FileLocation(nameof(Region_Flag_Icons))]
-    [SettingsUIGroupOrder(kButtonGroup, kToggleGroup, kSliderGroup, kDropdownGroup)]
-    [SettingsUIShowGroupName(kButtonGroup, kToggleGroup, kSliderGroup, kDropdownGroup)]
+    [FileLocation($"ModsSettings/{nameof(RegionFlagIcons)}/{nameof(RegionFlagIcons)}")]
+    [SettingsUIGroupOrder(kButtonGroup)]
+    [SettingsUIShowGroupName(kButtonGroup)]
     public class Setting : ModSetting
     {
         public const string kSection = "Main";
 
-        public const string kButtonGroup = "Button";
-        public const string kToggleGroup = "Toggle";
-        public const string kSliderGroup = "Slider";
-        public const string kDropdownGroup = "Dropdown";
+        public const string kButtonGroup = "Group";
+
+        [SettingsUIButton]
+        [SettingsUIConfirmation]
+        [SettingsUISection(kSection)]
+        public bool DeleteChangedIcons
+        {
+            set
+            {
+                Mod.DeleteChangedIcons();
+            }
+        }
 
         public Setting(IMod mod) : base(mod)
         {
         }
 
-        [SettingsUISection(kSection, kButtonGroup)]
-        public bool Button
-        {
-            set { Mod.log.Info("Button clicked"); }
-        }
-
-        [SettingsUIButton]
-        [SettingsUIConfirmation]
-        [SettingsUISection(kSection, kButtonGroup)]
-        public bool ButtonWithConfirmation
-        {
-            set { Mod.log.Info("ButtonWithConfirmation clicked"); }
-        }
-
-        [SettingsUISection(kSection, kToggleGroup)]
-        public bool Toggle { get; set; }
-
-        [SettingsUISlider(min = 0, max = 100, step = 1, scalarMultiplier = 1, unit = Unit.kDataMegabytes)]
-        [SettingsUISection(kSection, kSliderGroup)]
-        public int IntSlider { get; set; }
-
-        [SettingsUIDropdown(typeof(Setting), nameof(GetIntDropdownItems))]
-        [SettingsUISection(kSection, kDropdownGroup)]
-        public int IntDropdown { get; set; }
-
-        [SettingsUISection(kSection, kDropdownGroup)]
-        public SomeEnum EnumDropdown { get; set; } = SomeEnum.Value1;
-
-
-        public DropdownItem<int>[] GetIntDropdownItems()
-        {
-            var items = new List<DropdownItem<int>>();
-
-            for (var i = 0; i < 3; i += 1)
-            {
-                items.Add(new DropdownItem<int>()
-                {
-                    value = i,
-                    displayName = i.ToString(),
-                });
-            }
-
-            return items.ToArray();
-        }
-
         public override void SetDefaults()
         {
             throw new System.NotImplementedException();
-        }
-
-        public enum SomeEnum
-        {
-            Value1,
-            Value2,
-            Value3,
         }
     }
 
@@ -96,59 +52,19 @@ namespace Region_Flag_Icons
         {
             return new Dictionary<string, string>
             {
-                { m_Setting.GetSettingsLocaleID(), "Region_Flag_Icons" },
+                { m_Setting.GetSettingsLocaleID(), "Region Flag Icons" },
                 { m_Setting.GetOptionTabLocaleID(Setting.kSection), "Main" },
 
-                { m_Setting.GetOptionGroupLocaleID(Setting.kButtonGroup), "Buttons" },
-                { m_Setting.GetOptionGroupLocaleID(Setting.kToggleGroup), "Toggle" },
-                { m_Setting.GetOptionGroupLocaleID(Setting.kSliderGroup), "Sliders" },
-                { m_Setting.GetOptionGroupLocaleID(Setting.kDropdownGroup), "Dropdowns" },
-
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Button)), "Button" },
+                { m_Setting.GetOptionGroupLocaleID(Setting.kButtonGroup), "Settings" },
+                {m_Setting.GetOptionLabelLocaleID(nameof(Setting.DeleteChangedIcons)), "Uninstall"},
                 {
-                    m_Setting.GetOptionDescLocaleID(nameof(Setting.Button)),
-                    $"Simple single button. It should be bool property with only setter or use [{nameof(SettingsUIButtonAttribute)}] to make button from bool property with setter and getter"
-                },
-
-                {
-                    m_Setting.GetOptionLabelLocaleID(nameof(Setting.ButtonWithConfirmation)), "Button with confirmation"
+                    m_Setting.GetOptionDescLocaleID(nameof(Setting.DeleteChangedIcons)),
+                    $"This will remove the changed thumbnails from your AIL folder. Do this before uninstalling the mod, otherwise it will not have an effect."
                 },
                 {
-                    m_Setting.GetOptionDescLocaleID(nameof(Setting.ButtonWithConfirmation)),
-                    $"Button can show confirmation message. Use [{nameof(SettingsUIConfirmationAttribute)}]"
+                    m_Setting.GetOptionWarningLocaleID(nameof(Setting.DeleteChangedIcons)),
+                    $"Uninstall the icons? Please make sure to also remove the mod from your playset to fully uninstall it"
                 },
-                {
-                    m_Setting.GetOptionWarningLocaleID(nameof(Setting.ButtonWithConfirmation)),
-                    "is it confirmation text which you want to show here?"
-                },
-
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Toggle)), "Toggle" },
-                {
-                    m_Setting.GetOptionDescLocaleID(nameof(Setting.Toggle)),
-                    $"Use bool property with setter and getter to get toggable option"
-                },
-
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.IntSlider)), "Int slider" },
-                {
-                    m_Setting.GetOptionDescLocaleID(nameof(Setting.IntSlider)),
-                    $"Use int property with getter and setter and [{nameof(SettingsUISliderAttribute)}] to get int slider"
-                },
-
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.IntDropdown)), "Int dropdown" },
-                {
-                    m_Setting.GetOptionDescLocaleID(nameof(Setting.IntDropdown)),
-                    $"Use int property with getter and setter and [{nameof(SettingsUIDropdownAttribute)}(typeof(SomeType), nameof(SomeMethod))] to get int dropdown: Method must be static or instance of your setting class with 0 parameters and returns {typeof(DropdownItem<int>).Name}"
-                },
-
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.EnumDropdown)), "Simple enum dropdown" },
-                {
-                    m_Setting.GetOptionDescLocaleID(nameof(Setting.EnumDropdown)),
-                    $"Use any enum property with getter and setter to get enum dropdown"
-                },
-
-                { m_Setting.GetEnumValueLocaleID(Setting.SomeEnum.Value1), "Value 1" },
-                { m_Setting.GetEnumValueLocaleID(Setting.SomeEnum.Value2), "Value 2" },
-                { m_Setting.GetEnumValueLocaleID(Setting.SomeEnum.Value3), "Value 3" },
             };
         }
 
